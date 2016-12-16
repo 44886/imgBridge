7a45d74dc19a8465822d3ee8d9dfa24b4abc220d
风云酷小子 <boy@44886.com>
2016-12-16 21:29:59 +0800

    update

---
+++ imgBridge.js
@@ -0,0 +1,31 @@
+/**
+ *
+ *  Put the code into Express routers.
+ *  Before , You should install the module "superagent" .
+ *
+ */
+
+
+
+
+res.writeHead(200, {
+    'Content-Type': 'image/*'
+});
+let url = req.query.url;
+if (!url) {
+    res.send("");
+    return false;
+}
+superagent.get(req.query.url)
+    .set('Referer', '')
+    .set("User-Agent",
+        'User-Agent:Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36 Core/1.47.933.400 QQBrowser/9.4.8699.400'
+    )
+    .end(function(err, result) {
+        if (err) {
+            //res.send(err);
+            return false;
+        }
+        res.end(result.body);
+        return;
+    });
---
+++ imgBridge.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ *
+ * Put the code into a file without anyother thing.
+ * Put the fonts file near this file.
+ *
+ *
+ */
+
+  header("Content-Type:image/jpeg");
+        $string = $_SERVER["QUERY_STRING"];
+        /*换一张空白图片，如果遇到错误，需要用上*/
+        $im = imagecreate(600, 300);
+        $black = imagecolorallocate($im, 100, 100, 100);//图片背景
+        $white = imagecolorallocate($im, 255, 255, 255);
+        /*获取图片的真实地址*/
+        $url = strstr($string, "http");
+        if (!$url) {
+            imagettftext($im, 18, 0, 200, 100, $white, "字体文件的路径（可以相对本文件）", "Error 001");
+            imagettftext($im, 14, 0, 150, 150, $white, "字体文件的路径（可以相对本文件）", "请在参数中输入图片的绝对地址。");
+            imagepng($im);
+            exit();
+        }
+        @$imgString = $this->urlOpen($url);
+        if ($imgString == "") {
+            imagettftext($im, 18, 0, 200, 100, $white, "字体文件的路径（可以相对本文件）", "Error 002");
+            imagettftext($im, 14, 0, 70, 150, $white, "字体文件的路径（可以相对本文件）", "加载远程图片失败，请确认图片的地址能正常访问。");
+            imagepng($im);
+            exit();
+        }
+        /*如果没有错误*/
+        $im = imagecreatefromstring($imgString);
+        $white = imagecolorallocate($im, 255, 255, 255);
+        /*加上水印*/
+        imagettftext($im, 12, 0, 20, 20, $white, "字体文件的路径（可以相对本文件）", "水印的文字1");
+        imagettftext($im, 12, 0, 5, 35, $white, "字体文件的路径（可以相对本文件）", "水印（可以写你的网址）);
+        imagepng($im);
+?>
